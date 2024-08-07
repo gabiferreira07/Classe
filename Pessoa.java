e66f3127db8d53af56291ec54087dac8a4db3c5c
Gabrielli Ferreira <gabriellirodriguesferreira@gmail.com>
2024-08-07 12:29:33 -0300

    Classe Pessoa

---
+++ Pessoa.java
@@ -0,0 +1,47 @@
+package Classe;
+
+public class Pessoa {
+	String nome;
+	int idade;
+	String endereco;
+	String profissao;
+	String CPF;
+	String RG;
+
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getProfissao() {
+		return profissao;
+	}
+	public void setProfissao(String profissao) {
+		this.profissao = profissao;
+	}
+	public String getCPF() {
+		return cpf;
+	}
+	public void setCPF(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getRG() {
+		return rg;
+	}
+	public void setRG(String rg) {
+		this.rg = rg;
+	}
+}
